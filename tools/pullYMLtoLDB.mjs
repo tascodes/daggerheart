6672fa467c2c55b9c9c9f0fa211d36201d23edd4
WBHarry <89362246+WBHarry@users.noreply.github.com>
2025-05-25 01:33:07 +0200

    Fixed so Pack handling works recursively for deep compendium folders (#47)

--- tools/pullYMLtoLDB.mjs
+++ tools/pullYMLtoLDB.mjs
@@ -4,9 +4,28 @@ import { promises as fs } from 'fs';
 const MODULE_ID = process.cwd();
 const yaml = false;
 
-const packs = await fs.readdir('./src/packs');
+const packs = await deepGetDirectories('./packs');
+console.log(packs);
 for (const pack of packs) {
     if (pack === '.gitattributes') continue;
     console.log('Packing ' + pack);
-    await compilePack(`${MODULE_ID}/src/packs/${pack}`, `${MODULE_ID}/packs/${pack}`, { yaml });
+    await compilePack(`${MODULE_ID}/src/${pack}`, `${MODULE_ID}/${pack}`, { yaml });
+}
+
+async function deepGetDirectories(distPath) {
+    const dirr = await fs.readdir('src/' + distPath);
+    const dirrsWithSub = [];
+    for (let file of dirr) {
+        const stat = await fs.stat('src/' + distPath + '/' + file);
+        if (stat.isDirectory()) {
+            const deeper = await deepGetDirectories(distPath + '/' + file);
+            if (deeper.length > 0) {
+                dirrsWithSub.push(...deeper);
+            } else {
+                dirrsWithSub.push(distPath + '/' + file);
+            }
+        }
+    }
+
+    return dirrsWithSub;
 }
--- tools/pushLDBtoYML.mjs
+++ tools/pushLDBtoYML.mjs
@@ -5,11 +5,13 @@ import path from 'path';
 const MODULE_ID = process.cwd();
 const yaml = false;
 
-const packs = await fs.readdir('./packs');
+// const packs = await fs.readdir('./packs');
+const packs = await deepGetDirectories('./packs');
+console.log(packs);
 for (const pack of packs) {
     if (pack === '.gitattributes') continue;
     console.log('Unpacking ' + pack);
-    const directory = `./src/packs/${pack}`;
+    const directory = `./src/${pack}`;
     try {
         for (const file of await fs.readdir(directory)) {
             await fs.unlink(path.join(directory, file));
@@ -18,7 +20,7 @@ for (const pack of packs) {
         if (error.code === 'ENOENT') console.log('No files inside of ' + pack);
         else console.log(error);
     }
-    await extractPack(`${MODULE_ID}/packs/${pack}`, `${MODULE_ID}/src/packs/${pack}`, {
+    await extractPack(`${MODULE_ID}/${pack}`, `${MODULE_ID}/src/${pack}`, {
         yaml,
         transformName
     });
@@ -33,4 +35,24 @@ function transformName(doc) {
     const prefix = ['actors', 'items'].includes(type) ? doc.type : type;
 
     return `${doc.name ? `${prefix}_${safeFileName}_${doc._id}` : doc._id}.${yaml ? 'yml' : 'json'}`;
+}
+
+async function deepGetDirectories(distPath) {
+    const dirr = await fs.readdir(distPath);
+    const dirrsWithSub = [];
+    for (let file of dirr) {
+        const stat = await fs.stat(distPath + '/' + file);
+        if (stat.isDirectory()) {
+            if (file === 'packs') continue;
+
+            const deeper = await deepGetDirectories(distPath + '/' + file);
+            if (deeper.length > 0) {
+                dirrsWithSub.push(...deeper);
+            } else {
+                dirrsWithSub.push(distPath + '/' + file);
+            }
+        }
+    }
+
+    return dirrsWithSub;
 }
